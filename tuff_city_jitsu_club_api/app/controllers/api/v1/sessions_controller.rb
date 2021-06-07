c8cbb3d4475e94c1b927955e6061ad6741fb7d14
Seumas Finlayson <seumasfinlayson@googlemail.com>
2021-06-07 01:32:05 -0700

    Blank components on React side, and set methods for session controller

--- tuff_city_jitsu_club_api/app/controllers/api/v1/sessions_controller.rb
+++ tuff_city_jitsu_club_api/app/controllers/api/v1/sessions_controller.rb
@@ -1,2 +1,29 @@
 class Api::V1::SessionsController < ApplicationController
+    def create
+        user = User.find_by(email: params[:email])
+        puts params[:email]
+        puts user.id
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            puts session[:user_id]
+            render( 
+                json: { 
+                status: 200,
+                logged_in: true,
+                user: user
+             }
+            )
+        else
+            render(
+                json: { status: 404 },
+                status: 404
+            )
+        end
+    end
+
+    def destroy
+        session[:user_id] = nil
+        render(json: { status: 200}, status: 200 )
+    end
+
 end

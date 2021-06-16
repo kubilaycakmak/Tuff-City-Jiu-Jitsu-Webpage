7138ad6d3451cfd8f403479b33e9e2f30ed7c205
Seumas Finlayson <seumasfinlayson@googlemail.com>
2021-06-16 08:49:53 -0700

    More seed file adjustments

--- tuff_city_jitsu_club_api/db/seeds.rb
+++ tuff_city_jitsu_club_api/db/seeds.rb
@@ -11,6 +11,26 @@ PASSWORD = "justinbailey" # Inspired by a classic video game password
 User.delete_all
 Belt.delete_all
 BeltGrade.delete_all
+InstructorQualification.delete_all
+
+first_aid_qualifications = "First Aid & CPR" # Here we assume that a member will qualify for both First Aid and CPR simultaneously; therefore no need for an array, a string is sufficient
+instructor_qualification_array = ["Assistant Instructor", "Instructor", "Club Instructor"] # A member can only have one of these at a time, and each successive one supplants the last
+# Other rules which define grading criteria: a green belt going for purple belt needs Assistant Instructor qualification, a light blue going for dark blue needs Instructor, a dark blue
+# going for brown has first aid and CPR, a brown going for dan has Club Instructor. Also, first aid + CPR needs to be renewed every so many years and should go inactive here as in real life.
+
+# When considering pseudocode for this, we should presume that any member, even a beginner white belt can have a first aid qualification obtained for whatever reason (in or out of martial
+# arts), or may not, so they can randomly be assigned one. Also, we do not expect beginners to have instructor qualifications, so we can randomly assign Assistant Instructor to orange belts
+# at the earliest. Then Instructor to purple at the earliest, then Club Instructor to dark blue at the earliest (they have been mandated to teach solo in this fashion, before). This is all
+# arbirtrary and theoretical, but based on sound logic, and these users will be replaced by real ones in due course.
+
+# First, we set the qualifications for the real users Seumas and David
+
+
+instructor_qualification_array.each do |instructor_qualification|
+    InstructorQualification.create({
+        name: instructor_qualification
+    })
+end
 
 # Set up a user with admin privileges
 admin_user = User.create(
@@ -79,8 +99,32 @@ BeltGrade.create({user_id:2,belt_id:4}) # Likewise for David the 4th Kyu aka pur
     password: PASSWORD,
     dues_paid: true, # They must at least soon after signing up have their dues paid to be a regularly training member of the club
     owns_gi: true, # Ditto as with dues_paid
+    # instructor_qualification_id: instatiate this
+    has_first_aid_qualification: [true, false].sample
+    if has_first_aid_qualification == true
+        first_aid_achievement_date: Faker::Date.backward(days:365 *1)
+        first_aid_expiry_date: Faker::Date.forward(days:365 *2)
+    else
+        first_aid_achievement_date: "",
+        first_aid_expiry_date: "", # Null dates?
+    end
     # training_bubble_id: "random"
     )
+end
+
+
+if belt_id >= 6
+    # Randomly assign orange belt and above the Assistant Instructor qualification (note: a purple belt MUST have this)
+    # Id 1
+end
+
+if belt_id >= 4
+    # Randomly assign purple belt and above the Instructor qualification (note: a dark blue belt MUST have at least this)
+end
+
+if belt_id >= 2
+    # Randomly assign dark blue and brown belts Club Instructor (note, if they don't have this, we at least expect them to have Instructor instead, and then they MUST
+    # have Club Instructor for grading to black belt)
 end
 
 users = User.all
@@ -97,45 +141,12 @@ belts = Belt.all
     })
 end
 
-
-first_aid_qualifications = "First Aid & CPR" # Here we assume that a member will qualify for both First Aid and CPR simultaneously; therefore no need for an array, a string is sufficient
-instructor_qualification_array = ["Assistant Instructor", "Instructor", "Club Instructor"] # A member can only have one of these at a time, and each successive one supplants the last
-# Other rules which define grading criteria: a green belt going for purple belt needs Assistant Instructor qualification, a light blue going for dark blue needs Instructor, a dark blue
-# going for brown has first aid and CPR, a brown going for dan has Club Instructor. Also, first aid + CPR needs to be renewed every so many years and should go inactive here as in real life.
-
-# When considering pseudocode for this, we should presume that any member, even a beginner white belt can have a first aid qualification obtained for whatever reason (in or out of martial
-# arts), or may not, so they can randomly be assigned one. Also, we do not expect beginners to have instructor qualifications, so we can randomly assign Assistant Instructor to orange belts
-# at the earliest. Then Instructor to purple at the earliest, then Club Instructor to dark blue at the earliest (they have been mandated to teach solo in this fashion, before). This is all
-# arbirtrary and theoretical, but based on sound logic, and these users will be replaced by real ones in due course.
-
-# First, we set the qualifications for the real users Seumas and David
-
-
-instructor_qualification_array.each do |instructor_qualification|
-    InstructorQualification.create({
-        name: instructor_qualification
-    })
-end
-
-if belt_id >= 6
-    # Randomly assign orange belt and above the Assistant Instructor qualification (note: a purple belt MUST have this)
-    # Id 1
-end
-
-if belt_id >= 4
-    # Randomly assign purple belt and above the Instructor qualification (note: a dark blue belt MUST have at least this)
-end
-
-if belt_id >= 2
-    # Randomly assign dark blue and brown belts Club Instructor (note, if they don't have this, we at least expect them to have Instructor instead, and then they MUST for grading to black belt)
-end
-
-
+# Chicken and egg problem... what comes first, the users (and their belt_ids) or their instructor qualifications?
 
 puts Cowsay.say("Generated #{User.count} users", :sheep) 
 puts Cowsay.say("Generated #{BeltGrade.count} belt grades", :cow) 
 puts Cowsay.say("Generated #{Belt.count} belt colours", :beavis) 
-puts Cowsay.say("Generated #{InstructorQualifcation.count} belt colours", :frogs) 
+puts Cowsay.say("Generated #{InstructorQualification.count} belt colours", :frogs) 
 
 
 puts "Login with #{admin_user.email} and password of '#{PASSWORD}'"
